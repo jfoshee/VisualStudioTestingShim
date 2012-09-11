539521bc0a132fe7252cf3b38acc456dfb1ca3eb
Jacob Foshee <jacobf@gmail.com>
2012-09-11 13:48:14 -0500

    Initial commit

---
+++ VisualStudioTestingShim/Attributes.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
+{
+    public class TestClassAttribute : NUnit.Framework.TestFixtureAttribute {}
+
+    public class TestMethodAttribute : NUnit.Framework.TestAttribute {}
+
+    public class ExpectedExceptionAttribute : NUnit.Framework.ExpectedExceptionAttribute 
+    {
+        public ExpectedExceptionAttribute(Type exceptionType) : base(exceptionType) { }
+    }
+}
